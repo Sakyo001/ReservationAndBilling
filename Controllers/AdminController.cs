abafec60a1982505e3711f7675e315fd71bf75fa
unknown <masagnayjohnroel11@gmail.com>
2024-11-08 20:03:31 +0800

    hello

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Reservation.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult SignIn()
+        {
+            return View("~/Views/Admin/SignIn.cshtml");
+        }
+
+        public ActionResult RoomsManagement()
+        {
+            return View("~/Views/Admin/RoomsManagement.cshtml");
+        }
+    }
+}
